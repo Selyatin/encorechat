200ec19e1a3d6e464d2dcef1577e0209c949e826
Selyatin <selyatinismet@gmail.com>
2020-11-14 08:05:11 +0200

    Encryption added and further optimized

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,51 +1,64 @@
+extern crate short_crypt;
+
 use std::env::args;
-use std::io::Read;
-use std::io::Write;
-use std::net::TcpStream;
-use std::thread::spawn;
+use std::io::{Read, Write};
+use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
 use std::process::exit;
-use std::net::ToSocketAddrs;
-use std::net::SocketAddr;
-use std::thread::sleep;
+use std::sync::Arc;
+use std::thread::{sleep, spawn};
 use std::time::Duration;
 
+use short_crypt::ShortCrypt;
 fn main() {
     let argv: Vec<String> = args().collect();
-    let addr = argv[1].to_owned().to_socket_addrs().expect("Unable to resolve domain name").collect::<Vec<SocketAddr>>()[0];
+    let addr = argv[1]
+        .to_owned()
+        .to_socket_addrs()
+        .expect("Unable to resolve domain name")
+        .collect::<Vec<SocketAddr>>()[0];
+    let crypter = Arc::new(ShortCrypt::new(&argv[2]));
+    let crypter_clone = crypter.clone();
     let mut stream = TcpStream::connect(addr).expect("Couldn't connect to the server");
 
     println!("Nickname sent successfully, entering into an interactive loop.");
-    
+
     let mut stream_clone = stream.try_clone().expect("Couldn't clone stream");
-    spawn(move || {
-        loop {
-            let mut buff: Vec<u8> = vec![0; 512];
+    spawn(move || loop {
+        let mut buff: Vec<u8> = vec![0 as u8; 1024];
 
-            let err = stream_clone.read_exact(&mut buff).is_err();
-
-            if err {
-                println!("Couldn't read from server, exiting");
-                exit(1);
-            }
-
-            println!("{}", String::from_utf8(buff).unwrap_or(String::from("Couldn't unwrap message")));
-            sleep(Duration::from_millis(100));
+        if stream_clone.read(&mut buff).is_err() {
+            println!("Couldn't read from server, exiting");
+            exit(1);
         }
+        println!("{}", buff.len());
+        println!("{}", buff.len());
+        buff.retain(|byte| byte != &u8::MIN);
+        let buff_string = String::from_utf8(buff).unwrap();
+        let decrypted = crypter_clone
+            .decrypt_url_component(buff_string)
+            .unwrap_or("Couldn't decrypt message".as_bytes().to_vec());
+
+        let decrypted_string = String::from_utf8_lossy(&decrypted);
+        println!("{}", decrypted_string);
+        sleep(Duration::from_millis(100));
     });
 
     loop {
         let stdin = std::io::stdin();
-        let mut buff = String::from(&argv[2]);
+        let mut buff = String::from(&argv[3]);
         buff += ": ";
         stdin
             .read_line(&mut buff)
             .expect("Error while trying to read line from stdin");
+        let encrypted: Vec<u8> = crypter
+            .encrypt_to_url_component(&buff)
+            .as_bytes()
+            .to_owned();
         if buff.len() > 0 {
             stream
-                .write_all(buff.as_bytes())
+                .write_all(&encrypted)
                 .expect("Error while sending message");
         }
-
         sleep(Duration::from_millis(100));
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,62 +1,82 @@
-use std::net::{TcpStream, TcpListener, Shutdown};
-use std::vec::Vec;
-use std::sync::{mpsc::{channel}, Arc, Mutex};
 use std::env::args;
-use std::thread::{spawn};
 use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{mpsc::channel, Arc, Mutex};
+use std::thread::spawn;
+use std::vec::Vec;
 
-fn sleep(){
+fn sleep() {
     std::thread::sleep(std::time::Duration::from_millis(100));
 }
 
-fn main(){
+fn main() {
     let args: Vec<String> = args().collect();
+
     let listener = TcpListener::bind("0.0.0.0:".to_owned() + &args[1]).expect("Couldn't bind");
-    listener.set_nonblocking(true).expect("Couldn't set nonblocking");
+
+    listener
+        .set_nonblocking(true)
+        .expect("Couldn't set nonblocking");
+
     let clients_lock = Arc::new(Mutex::new(Vec::<TcpStream>::new()));
     let clients_lock_clone = clients_lock.clone();
+
     let (sender, receiver) = channel::<Vec<u8>>();
+
     println!("---- Encorechat ----");
+
     loop {
-        println!("Clients: {:?}", clients_lock.lock().expect("Couldn't lock client_lock in println"));
-        if let Ok((mut stream, _)) = listener.accept(){
+        if let Ok((mut stream, _)) = listener.accept() {
+            
+            println!("New Client: {:?}", stream);
+            
             let mut clients = clients_lock.lock().expect("Couldn't lock clients_lock");
-            clients.push(stream.try_clone().expect("Couldn't clone stream to put it inside clients Vector"));
+
+            clients.push(
+                stream
+                    .try_clone()
+                    .expect("Couldn't clone stream to put it inside clients Vector"),
+            );
+            
             let sender_clone = sender.clone();
-
+            
             spawn(move || loop {
-                let mut buff: Vec<u8> = vec![0 as u8; 2056];
-    
-                if stream.read(&mut buff).is_err(){
+                let mut buff: Vec<u8> = vec![0 as u8; 512];
+
+                if stream.read(&mut buff).is_err() {
                     println!("Error while reading from stream: {:?}", stream);
-                    break
+                    break;
                 }
 
-                if buff[0] != 0 {
-                    sender_clone.send(buff);
+                buff.retain(|byte| byte != &u8::MIN);
+
+                if buff.len() == 0 {
+                    break;
                 }
+
+                println!("{}", String::from_utf8_lossy(&buff));
+                sender_clone.send(buff).expect("Sender failed");
 
                 sleep();
             });
         }
 
         if let Ok(msg) = receiver.try_recv() {
-            let mut clients = clients_lock_clone.lock().expect("Couldn't lock client_lock in receiver");
-            let i = 0;
-            while i < clients.len(){
-                let mut client = &mut clients[i];
-                match client.write(&msg) {
-                    Ok(_) => {
-                        continue;
-                    },
-                    
-                    Err(_) => {
-                        clients.remove(i);
-                    }
+            let mut clients = clients_lock_clone
+                .lock()
+                .expect("Couldn't lock client_lock in receiver");
+
+            let mut i = 0;
+
+            while i < clients.len() {
+                let client = &mut clients[i];
+                if client.write(&msg).is_err() {
+                    clients.remove(i);
                 }
+                i += 1;
             }
         }
 
-    sleep();    
+        sleep();
     }
 }
